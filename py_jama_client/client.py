54709a1fcb549fa346f0121fcf8943e9d425ab44
Vladimir Kulev <me@lightoze.net>
2026-04-21 02:02:59 +0300

    Fix get_available_endpoints

--- py_jama_client/client.py
+++ py_jama_client/client.py
@@ -84,7 +84,7 @@ class JamaClient:
 
     def get_available_endpoints(self):
         try:
-            response = self.__session.get("")
+            response = self.__session.get(self.__host_name)
         except CoreException as err:
             py_jama_client_logger.error(err)
             raise APIException(str(err))
